ce1f9b60ff2859b6716480900e4e7f45c6977a98
tazlambert <54355516+tazlambert@users.noreply.github.com>
2020-05-14 11:46:58 +0700

    Update download.sh

--- terraform/download.sh
+++ terraform/download.sh
@@ -1,19 +1,19 @@
-[ -d WebLogic-Cloud-Workshop ] && rm -r WebLogic-Cloud-Workshop
+[ -d weblogic-move-improve ] && rm -r weblogic-move-improve
 
-printf "\nCreating directory WebLogic-Cloud-Workshop ...\n\n"
+printf "\nCreating directory weblogic-move-improve ...\n\n"
 
-mkdir WebLogic-Cloud-Workshop
+mkdir weblogic-move-improve
 
-cd WebLogic-Cloud-Workshop
+cd weblogic-move-improve
 
-printf "Downloading terraform scripts in ~/WebLogic-Cloud-Workshop ...\n\n"
+printf "Downloading terraform scripts in ~/weblogic-move-improve ...\n\n"
 
-curl https://raw.githubusercontent.com/StephaneMoriceau/WebLogic-Cloud-Workshop/master/terraform/main.tf --output main.tf
+curl https://raw.githubusercontent.com/tazlambert/weblogic-move-improve/master/terraform/main.tf --output main.tf
 
-curl https://raw.githubusercontent.com/StephaneMoriceau/WebLogic-Cloud-Workshop/master/terraform/provider.tf --output provider.tf
+curl https://raw.githubusercontent.com/tazlambert/weblogic-move-improve/master/terraform/provider.tf --output provider.tf
 
-curl https://raw.githubusercontent.com/StephaneMoriceau/WebLogic-Cloud-Workshop/master/terraform/terraform.tfvars.example --output terraform.tfvars.example
+curl https://raw.githubusercontent.com/tazlambert/weblogic-move-improve/master/terraform/terraform.tfvars.example --output terraform.tfvars.example
 
-curl https://raw.githubusercontent.com/StephaneMoriceau/WebLogic-Cloud-Workshop/master/terraform/variables.tf --output variables.tf
+curl https://raw.githubusercontent.com/tazlambert/weblogic-move-improve/master/terraform/variables.tf --output variables.tf
 
 printf "\nDownload complete.\n\n"
